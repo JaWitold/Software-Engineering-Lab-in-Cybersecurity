03b94ee37e4dd9270c37b63842f3cfbe8d4db0be
Witold <47141012+JaWitold@users.noreply.github.com>
2023-12-12 20:22:40 +0100

    Revert "[Task-3] Python NrSign & Solidity NrVerify (#19)"
    This reverts commit 8903e44e01a677e723101286f6f52423b09909d4.

--- python/nr_verify/schemas/node_ring_schnorr.py
+++ python/nr_verify/schemas/node_ring_schnorr.py
@@ -172,3 +172,14 @@ class NodeRingSchnorr:
             )), byteorder="big") % self.GEN_ORDER
             sum_ = add(sum_, add(ephemeral_randomness[i], multiply(ext_public_key, hash_)))
         return prod and eq(multiply(G1, master_sum), sum_)
+
+
+if __name__ == '__main__':
+    # Example usage
+    PRIVKEY = 34783947491279721981739821
+    s = NodeRingSchnorr()
+    seal = s.nr_sign(PRIVKEY, 123, [multiply(G1, 2), multiply(G1, 3)])
+    # signature = s.nr_sign(PRIVKEY, 123, [])
+    print(seal)
+    print("result: ", s.nr_verify(123, seal))
+    print("result: ", s.nr_verify(1233, seal))
